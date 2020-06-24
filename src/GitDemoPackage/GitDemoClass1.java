35459841b24e700532dc17b87e8eb8d953be2863
jmnsanjay <jmn.sanjay@gmail.com>
2020-06-24 16:07:24 -0400

    First commit by SR

---
+++ src/GitDemoPackage/GitDemoClass1.java
@@ -0,0 +1,14 @@
+package GitDemoPackage;
+
+public class GitDemoClass1 {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=3;
+		b=5;
+		c=a+b;
+		System.out.println("Sum of a and b is "+c);
+
+	}
+
+}
